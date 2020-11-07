f0cd8ba1aa36ce3605ef0dd9961a40d5f669adce
doongjun <66319788+doongjun@users.noreply.github.com>
2020-11-07 23:12:20 +0900

    #5
    create apps , lists, reservations, reviews, conversations

--- core/models.py
+++ core/models.py
@@ -4,8 +4,10 @@ from django.db import models
 class TimeStampedModel(models.Model):
 
     """ Time Stamped Model """
-
+    
+    #필드가 모델을 저장할 때 date, time 기록
     created = models.DateTimeField(auto_now_add=True)
+    #필드가 모델을 생성할 때마다 수시로 업데이트
     updated = models.DateTimeField(auto_now=True)
 
     class Meta:
