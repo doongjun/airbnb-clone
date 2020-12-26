9e50f3b3d2ddc1eda2f8b44e8498f8a6eaa2c430
doongjun <66319788+doongjun@users.noreply.github.com>
2020-12-26 18:03:05 +0900

    #19
    setting tailwind css

--- config/settings.py
+++ config/settings.py
@@ -133,9 +133,11 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
 AUTH_USER_MODEL = "users.User"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
+MEDIA_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = "/media/"
 
